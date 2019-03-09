bb10c1188444ccd4c5e327c12f73a95b9e451216
Mishkovec <neilsa@mail.ru>
2019-03-09 13:45:07 +0300

    add code love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+   var result = 0;
+   preferences.unshift(0);
+   //alert(data);
+   for(let i = 1; i < preferences.length; i++){
+       let j = preferences[i];
+       let k = preferences[j];
+       
+       result += i == preferences[k];
+       //alert(data[k]);
+   }
+   let res =Math.floor(result/3);
+   return res;
+}
